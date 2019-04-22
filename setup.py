831b81a305695cd32cd0df893667473c6c44fad4
Mikhail Kurenkov <kurenkov1995@mail.ru>
2019-04-22 12:31:18 +0300

    Add setup file for version 0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='skrobotlibs_utility',
+    version='0.1',
+    packages=['numpy'],
+    url='',
+    license='',
+    author='mikhail',
+    author_email='Mikhail.Kurenkov@skoltech.ru',
+    description=''
+)
