62ae452f8bbe14fa9cf96028226abcd6df6c82ee
nickzherdev <nikolay.zherdev@gmail.com>
2019-07-31 15:31:26 +0300

    add description to l2g, g2l and fs fuctions

--- robot_conversions.py
+++ robot_conversions.py
@@ -1,3 +1,9 @@
+"""
+Conversions utils for robotics.
+Authors: Mikhail Kurenkov (Mikhail.Kurenkov@skoltech.ru),
+        Nikolay Zherdev (Nikolay.Zherdev@skoltech.ru)
+"""
+
 import numpy as np
 import sys
 import cv2
@@ -8,12 +14,88 @@ if sys.version_info.major == 2:
     import rospy
 
 
+def rotate(points, angle):
+    s, c = np.sin(angle), np.cos(angle)
+    r = np.array([[c, s], [-s, c]])
+    return np.dot(points[:, :2], r)
+# poses_[:, :2] = np.dot(poses_[:, :2], r)
+
+
+def convert2xy(scan, fov=260, min_dist=0.02):
+    """Converts scan data to list of XY points descarding values with distances
+    less than `min_dist`.
+
+    Parameters
+    ----------
+    scan : array-like
+        List of scan measurments in mm
+    fov : scalar, optional
+        Field Of View of the sensor in degrees
+    min_dist : scalar, optional
+        Minimal distance of measurment in mm for filtering out values
+
+    Returns
+    -------
+    points : ndarray
+        List of XY points
+    """
+    angles = np.radians(np.linspace(-fov/2, fov/2, len(scan)))
+    points = np.vstack([scan*np.cos(angles), scan*np.sin(angles)]).T
+    return points[scan>min_dist]
+
+
+def convert2map(pose, points, map_pix, map_size, prob):
+    """Converts list of XY points to 2D array map in which each pixel denotes
+    probability of pixel being occupied.
+
+    Parameters
+    ----------
+    pose : ndarray
+        XY coordinates of the robot in the map reference frame
+    points : ndarray
+        List of XY points measured by sensor in the map reference frame
+    map_pix : int
+        Size of map pixel in m
+    map_size : tuple
+        Size of the map in pixels
+    prob : float
+        Probability
+
+
+    Returns
+    -------
+    map : ndarray
+        2D array representing map with dtype numpy.float32
+    """
+    zero = (pose//map_pix).astype(np.int32)
+    pixels = (points//map_pix).astype(np.int32)
+    mask = (pixels[:, 0] >= 0) & (pixels[:, 0] < map_size[0]) & \
+           (pixels[:, 1] >= 0) & (pixels[:, 1] < map_size[1])
+    pixels = pixels[mask]
+    img = Image.new('L', (map_size[1], map_size[0]))
+    draw = ImageDraw.Draw(img)
+    zero = (zero[1], zero[0])
+    for p in set([(q[1], q[0]) for q in pixels]):
+        draw.line([zero, p], fill=1)
+    data = -np.fromstring(img.tobytes(), np.int8).reshape(map_size)
+    data[pixels[:, 0], pixels[:, 1]] = 1
+    return 0.5 + prob*data.astype(np.float32)
+
+
 def cvt_local2global(local_point, src_point):
     """
-    Convert points from local frame to global
-    :param local_point: A local point or array of local points that must be converted 1-D np.array or 2-D np.array
-    :param src_point: A
-    :return:
+    Convert local_point (defined in robot's local frame) to the frame where src_point is defined (map, for ex).
+
+    For example, we have a robot located in one point (src_point param) and we want it to move half a meter back,
+    while keeping the same orientation. The easiest way to do it is to imagine that robot stays at [x=0, y=0, theta=0]
+    in his local frame and to substract 0.5 from corresponding axis.
+    Considering X axis is looking forward, we'll get [-0.5, 0, 0] - this is a local_point param
+    (map -- base)
+
+    :param local_point: A local point or array of local points that must be converted 1-D np.array or 2-D np.array,
+                        change in pos and orientation in local robot fame
+    :param src_point: Point from which robot starts moving
+    :return: coordinates [x, y, theta] where we want robot to be in src_points' frame
     """
     size = local_point.shape[-1]
     x, y, a = 0, 0, 0
@@ -34,6 +116,20 @@ def cvt_local2global(local_point, src_point):
 
 
 def cvt_global2local(global_point, src_point):
+    """
+    Returns coordinates of a global_point in src_point's frame.
+
+    For example, we have robot's position in map frame (global_point) and we have one of it's
+    previous positions in the same global space (src_point). The task is to find a delta between
+    these two points in local frame of src_point. (odom -- base)
+
+    Another example: once collision points are calculated in global frame, they need to be converted to robot's local
+    frame to check if they are inside "robot's collision ellipse"
+
+    :param global_point:
+    :param src_point:
+    :return:
+    """
     size = global_point.shape[-1]
     x1, y1, a1 = 0, 0, 0
     if size == 3:
@@ -53,6 +149,16 @@ def cvt_global2local(global_point, src_point):
 
 
 def find_src(global_point, local_point):
+    """
+    Transformation map looks as following: map --> odom --> base
+
+    For example, we have VICON ground truth robot position (global_point) and position by odometry in odom_frame,
+    and we need to find an error between them.
+
+    :param global_point:
+    :param local_point:
+    :return:
+    """
     x, y, a = local_point.T
     x1, y1, a1 = global_point.T
     A = (a1 - a) % (2 * np.pi)
